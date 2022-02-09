dd91ea046b65b2d3cbe946f738064cf9569e2799
caseysugden <66505022+caseysugden@users.noreply.github.com>
2022-02-09 16:09:05 -0700

    Fixed PROBLEM 2 syntax

--- lab.js
+++ lab.js
@@ -28,9 +28,9 @@ var brittanisBirthYear = 1986
 if (amysAge > brittanisAge) {
   console.log(`Amy is older.`);
 } else if (amysAge < brittanisAge) {
-  console.log(`Brittani is older.`)
+  console.log(`Brittani is older.`);
 } else {
-  console.log('Amy and Brittani are the same age')
+  console.log('Amy and Brittani are the same age');
 }
 
 
