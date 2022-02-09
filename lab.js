5b554e9e4aedbd9a33ab1aa5417057ca13809ec1
caseysugden <66505022+caseysugden@users.noreply.github.com>
2022-02-09 16:34:37 -0700

    Solved PROBLEM 7

--- lab.js
+++ lab.js
@@ -98,7 +98,9 @@ for (i = 1; i <= 10; i++) {
   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
 */
 
-//CODE HERE
+for (i = 10; i > 0; i--) {
+  console.log(`${i}`);
+}
 
 let score = 0
 let passingScore = 7
